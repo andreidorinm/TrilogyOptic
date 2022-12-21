6e3126d3503958546c491b05300845eaede33ec9
andreidorinm <andrei.dorin09@yahoo.com>
2022-12-22 00:44:10 +0200

    added generateJWT

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,40 +1,42 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Carousel, Image } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import Loader from './Loader'
-import Message from './Message'
-import { listTopProducts } from '../actions/productActions'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Carousel, Image } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "./Loader";
+import Message from "./Message";
+import { listTopProducts } from "../actions/productActions";
 
 const ProductCarousel = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const productTopRated = useSelector((state) => state.productTopRated)
-  const { loading, error, products } = productTopRated
+  const productTopRated = useSelector((state) => state.productTopRated);
+  const { loading, error, products } = productTopRated;
 
   useEffect(() => {
-    dispatch(listTopProducts())
-  }, [dispatch])
+    dispatch(listTopProducts());
+  }, [dispatch]);
 
   return loading ? (
     <Loader />
   ) : error ? (
-    <Message variant='danger'>{error}</Message>
+    <Message variant="danger">{error}</Message>
   ) : (
-    <Carousel pause='hover' className='bg-dark' id='gradient'>
+    <Carousel pause="hover" className="bg-dark" id="gradient">
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className='carousel-caption'>
-              <h2 style={{
-          textAlign: "center",
-          maxWidth: "950px",
-          margin: "0 auto",
-          padding: "10px",
-          marginTop: "-15px",
-          marginBottom: "10px"
-        }}>
+            <Carousel.Caption className="carousel-caption">
+              <h2
+                style={{
+                  textAlign: "center",
+                  maxWidth: "950px",
+                  margin: "0 auto",
+                  padding: "10px",
+                  marginTop: "-15px",
+                  marginBottom: "10px",
+                }}
+              >
                 {product.name} ({product.price}LEI)
               </h2>
             </Carousel.Caption>
@@ -42,7 +44,7 @@ const ProductCarousel = () => {
         </Carousel.Item>
       ))}
     </Carousel>
-  )
-}
+  );
+};
 
-export default ProductCarousel
+export default ProductCarousel;
