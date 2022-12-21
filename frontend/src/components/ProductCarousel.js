31a6c835f41c6e51e9f99a7a3db090586bd93be7
andreidorinm <andrei.dorin09@yahoo.com>
2022-12-22 00:50:29 +0200

    added single quotes

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Carousel, Image } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import Loader from "./Loader";
-import Message from "./Message";
-import { listTopProducts } from "../actions/productActions";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Carousel, Image } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from './Loader';
+import Message from './Message';
+import { listTopProducts } from '../actions/productActions';
 
 const ProductCarousel = () => {
   const dispatch = useDispatch();
@@ -29,12 +29,12 @@ const ProductCarousel = () => {
             <Carousel.Caption className="carousel-caption">
               <h2
                 style={{
-                  textAlign: "center",
-                  maxWidth: "950px",
-                  margin: "0 auto",
-                  padding: "10px",
-                  marginTop: "-15px",
-                  marginBottom: "10px",
+                  textAlign: 'center',
+                  maxWidth: '950px',
+                  margin: '0 auto',
+                  padding: '10px',
+                  marginTop: '-15px',
+                  marginBottom: '10px',
                 }}
               >
                 {product.name} ({product.price}LEI)
