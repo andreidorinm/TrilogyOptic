d79e5adbed06c5d74acddd9787720d9fd35e9954
andreidorinm <andrei.dorin09@yahoo.com>
2022-12-25 19:35:39 +0200

    fixing build

---
+++ frontend/build/precache-manifest.1a9e6bd381a1da716013b1f5913ef556.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df2bc4db73d84ecff420511536149df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6901cc70ad7f104917b2",
+    "url": "/static/css/main.62798b4c.chunk.css"
+  },
+  {
+    "revision": "1ad7c7e5746540d5c57b",
+    "url": "/static/js/2.54011197.chunk.js"
+  },
+  {
+    "revision": "c0fa58a94cdbab3827ec663ce548f3aa",
+    "url": "/static/js/2.54011197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6901cc70ad7f104917b2",
+    "url": "/static/js/main.5b60b01f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5b60b01f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  },
+  {
+    "revision": "627559e48b063def2c666554296af349",
+    "url": "/static/media/armani.627559e4.jpg"
+  },
+  {
+    "revision": "db1e0a23f06bb25b6591c5adb7aed4bb",
+    "url": "/static/media/baldinini.db1e0a23.jpg"
+  },
+  {
+    "revision": "518370c8def90575ce05cafecb27d87b",
+    "url": "/static/media/blumarine.518370c8.jpg"
+  },
+  {
+    "revision": "52fbee7588c443d3a88c58b5528901c2",
+    "url": "/static/media/boss.52fbee75.jpg"
+  },
+  {
+    "revision": "3de9dee05c01129eb3fd52a3fe09d887",
+    "url": "/static/media/burberry.3de9dee0.jpg"
+  },
+  {
+    "revision": "53555dba1beb3804e22d4dadf1a20bfd",
+    "url": "/static/media/bvlgari.53555dba.jpg"
+  },
+  {
+    "revision": "e65dcb4c3f511d78a122542f19417ce0",
+    "url": "/static/media/diesel.e65dcb4c.jpg"
+  },
+  {
+    "revision": "7a1c249b2b4948f57dec1b6ad98b651b",
+    "url": "/static/media/dior.7a1c249b.jpg"
+  },
+  {
+    "revision": "3147c24bd4794431435cdd8171ab032b",
+    "url": "/static/media/escada.3147c24b.jpg"
+  },
+  {
+    "revision": "05bf0ff7bdae10d4316f568190da47a8",
+    "url": "/static/media/ford.05bf0ff7.jpg"
+  },
+  {
+    "revision": "2dedd68478ad5317b7378c0f84755249",
+    "url": "/static/media/fossil.2dedd684.jpg"
+  },
+  {
+    "revision": "a33b109d3db4ed4246081e0b950b19b7",
+    "url": "/static/media/furla.a33b109d.jpg"
+  },
+  {
+    "revision": "3b34c2bff7b5f6cba7795d3599f85be4",
+    "url": "/static/media/gabbana.3b34c2bf.jpg"
+  },
+  {
+    "revision": "60aa73aa430df43eb1cb1740c7330985",
+    "url": "/static/media/gant.60aa73aa.jpg"
+  },
+  {
+    "revision": "4806721b67a854a2f97ea17b60e80438",
+    "url": "/static/media/herrera.4806721b.jpg"
+  },
+  {
+    "revision": "49ccba61f9fa8d8e1c6ebbbb71bffdfa",
+    "url": "/static/media/hilfiger.49ccba61.jpg"
+  },
+  {
+    "revision": "063ecc84c2902ac0e884d9b05a3b4582",
+    "url": "/static/media/kors.063ecc84.jpg"
+  },
+  {
+    "revision": "89c2b5a4de61d02ca148bf1e3987c8da",
+    "url": "/static/media/lloyd.89c2b5a4.jpg"
+  },
+  {
+    "revision": "e863d60c6997834e49c8ceea2706a6df",
+    "url": "/static/media/locatie.e863d60c.jpg"
+  },
+  {
+    "revision": "fff638073d2272a82dea1faac8c8bde7",
+    "url": "/static/media/marc.fff63807.jpg"
+  },
+  {
+    "revision": "87cdf372ed6648b28e512a4b148df0ea",
+    "url": "/static/media/max.87cdf372.jpg"
+  },
+  {
+    "revision": "4a787e93230b60226c6d4d7fe2667b4c",
+    "url": "/static/media/moschino.4a787e93.jpg"
+  },
+  {
+    "revision": "2ad8c3711ff35d4af83a9959f84d8fbf",
+    "url": "/static/media/persol.2ad8c371.jpg"
+  },
+  {
+    "revision": "d99bf06e27816dfef2a5b9ea1c51a4e2",
+    "url": "/static/media/prada.d99bf06e.jpg"
+  },
+  {
+    "revision": "094d80698fd29ef9378c5015e6792b5a",
+    "url": "/static/media/puma.094d8069.jpg"
+  },
+  {
+    "revision": "dd80ae0db748d91d1f333a28f5374bef",
+    "url": "/static/media/rodenstock.dd80ae0d.jpg"
+  },
+  {
+    "revision": "f8225903bd11d78ee84dd3ddadf23745",
+    "url": "/static/media/swarovski.f8225903.jpg"
+  },
+  {
+    "revision": "459f4a31dd4860015bef4836256f71ea",
+    "url": "/static/media/valentino.459f4a31.jpg"
+  },
+  {
+    "revision": "f3859a3d7bfaa8d08a6b80f062dcac00",
+    "url": "/static/media/victoria.f3859a3d.jpg"
+  },
+  {
+    "revision": "05a1fe65cb880ec51de22b6f3a35f688",
+    "url": "/static/media/vogue.05a1fe65.jpg"
+  }
+]);
